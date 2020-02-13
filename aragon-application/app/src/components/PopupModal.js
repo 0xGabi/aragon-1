b3729fc0cae697a1c6401adb7c1e5d391e638dcd
Benz <ruthamnong.thotsaphon@gmail.com>
2020-02-13 15:46:13 +0700

    Update modal for download process

--- aragon-application/app/src/components/PopupModal.js
+++ aragon-application/app/src/components/PopupModal.js
@@ -4,62 +4,30 @@ import React from 'react'
 import yaml from 'yaml-js'
 
 import { getAllVersions } from '../utils/APM'
+import { encodeEventSignature } from '../utils/Web3'
+import ProcessTemplate from '../utils/processTemplate'
 
 function PopupModal({ displayModal, closeModal, processData }) {
   const { installedApps } = useAragonApi()
 
-  const getAbi = async () => {
+  const getEventAbi = async () => {
     if (processData !== null) {
       const installedApp = installedApps.find(app => app.appAddress.toLowerCase() === processData.appAddress.toLowerCase())
       const version = await getAllVersions(installedApp.appId, installedApp.appImplementationAddress)
       if (!version) {
         throw new Error(`cannot find version for ${app.appImplementationAddress}`)
       }
-      const abiEvent = version.abi.find(abi => abi.name === processData.eventName)
-      return abiEvent
+      const eventAbi = version.abi.find(abi => abi.name === processData.eventName)
+      return eventAbi
     }
   }
 
   const downloadFile = async () => {
-    const abiEvent = await getAbi()
+    const eventAbi = await getEventAbi()
     const endpoint = 'ws://docker.for.mac.localhost:8545' // TODO: remove localhost endpoint
-    const createYaml = yaml.dump({
-      name: `${processData.appAddress}/${processData.index}`,
-      steps: [
-        {
-          type: 'trigger',
-          instance: {
-            src: 'https://github.com/mesg-foundation/service-ethereum-contract',
-            env: [`PROVIDER_ENDPOINT=${endpoint}`, `CONTRACT_ADDRESS=${processData.appAddress}`, `CONTRACT_ABI=[${JSON.stringify(abiEvent)}]`]
-          },
-          eventKey: 'event'
-        },
-        {
-          type: 'filter',
-          conditions: {
-            name: processData.eventName
-          }
-        },
-        {
-          type: 'task',
-          instance: {
-            src: 'https://github.com/mesg-foundation/service-webhook'
-          },
-          taskKey: 'call',
-          inputs: {
-            url: processData.url,
-            data: {
-              name: { key: 'name' },
-              blockHash: { key: 'blockHash' },
-              blockNumber: { key: 'blockNumber' },
-              transactionHash: { key: 'transactionHash' },
-              transactionIndex: { key: 'transactionIndex' },
-              data: { key: 'data' }
-            }
-          }
-        }
-      ]
-    })
+    const encode = await encodeEventSignature(eventAbi)
+    const temp = ProcessTemplate(processData, eventAbi, encode, endpoint)
+    const createYaml = yaml.dump(temp)
     const element = document.createElement('a')
     const file = new Blob([createYaml], { type: 'application/x-yaml' })
     element.href = URL.createObjectURL(file)
