e6b30946e42ac2becdbfb171809c0177ed78cbf4
Benz <ruthamnong.thotsaphon@gmail.com>
2020-02-25 17:37:05 +0700

    use Rinkeby network

--- aragon-application/app/src/utils/Web3.js
+++ aragon-application/app/src/utils/Web3.js
@@ -1,6 +1,11 @@
 import Web3 from 'web3'
 
-const provider = 'ws://localhost:8545'
+let provider = 'https://rinkeby.infura.io/v3/1aeb73da68804152b224a285ef96ce02'
+
+if (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined') {
+  provider = window['ethereum'] || window.web3.currentProvider
+  console.log('Web3 browser user detected')
+}
 
 export const web3 = new Web3(provider)
 
--- aragon-application/app/src/utils/ipfs-util.js
+++ aragon-application/app/src/utils/ipfs-util.js
@@ -1,6 +1,6 @@
 import ipfsAPI from 'ipfs-http-client'
 
-export var ipfs = ipfsAPI('http://ipfs.app.mesg.com:5001')
+export var ipfs = ipfsAPI('https://ipfs.app.mesg.com:5001')
 
 export async function save(content) {
   for await (const result of ipfs.add(content)) {
