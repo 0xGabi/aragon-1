4eeee60674ac7dcc941b8447cfeb5a59403aacbb
Benz <ruthamnong.thotsaphon@gmail.com>
2020-03-20 14:03:17 +0700

    Update script file

--- app/src/script.js
+++ app/src/script.js
@@ -4,35 +4,69 @@ import Aragon, { events } from '@aragon/api'
 
 const app = new Aragon()
 
-app.store(
-  async (state, { event }) => {
-    const nextState = { ...state }
+const retryEvery = async (callback, { initialRetryTimer = 1000, increaseFactor = 3, maxRetries = 3 } = {}) => {
+  const sleepTime = async time => new Promise(resolve => setTimeout(resolve, time))
+  let retryNum = 0
+  const attempt = async (retryTimer = initialRetryTimer) => {
     try {
-      switch (event) {
-        case events.SYNC_STATUS_SYNCING:
-          return { ...nextState, isSyncing: true }
-        case events.SYNC_STATUS_SYNCED:
-          return { ...nextState, isSyncing: false }
-        case 'Created':
-        case 'Disactivated':
-          return { ...nextState, processes: await getProcess() }
-        default:
-          return state
+      return await callback()
+    } catch (error) {
+      if (retryNum === maxRetries) {
+        throw error
       }
-    } catch (err) {
-      console.log(err)
-    }
-  },
-  {
-    init: initializeState()
-  }
-)
 
-/***********************
- *                     *
- *   Event Handlers    *
- *                     *
- ***********************/
+      ++retryNum
+      const nextRetryTime = retryTimer * increaseFactor
+      await sleepTime(nextRetryTime)
+      return attempt(nextRetryTime)
+    }
+  }
+  return attempt()
+}
+
+const call = () =>
+  retryEvery(async () => {
+    const size = await app.call('size').toPromise()
+    if (parseInt(size) !== 0) {
+      console.log(`size isn't zero`)
+      const setArr = new Array(size).fill(null).map((_, i) => app.call('getProcess', i))
+      const processList = await Promise.all(setArr)
+      console.log(processList)
+      return initialize({ size, processList })
+    }
+    console.log(`Can't get processList because size is ${size}`)
+    return initialize({})
+  })
+
+call()
+
+async function initialize(data) {
+  return app.store(
+    async (state, { blockNumber, event, returnValues, transactionHash }) => {
+      console.log(`blockNumber: ${blockNumber}, event: ${event}, transactionHash: ${transactionHash}, `, 'returnValues: ', returnValues)
+      const nextState = { ...state }
+      try {
+        switch (event) {
+          case events.SYNC_STATUS_SYNCING:
+            return { ...nextState, isSyncing: true }
+          case events.SYNC_STATUS_SYNCED:
+            return { ...nextState, isSyncing: false }
+          case 'Created':
+            return { ...nextState, processes: await getProcessUpdate() }
+          case 'Disactivated':
+            return { ...nextState, processes: await getProcessUpdate() }
+          default:
+            return state
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    {
+      init: initializeState()
+    }
+  )
+}
 
 function initializeState() {
   return async cachedState => {
@@ -43,7 +77,13 @@ function initializeState() {
   }
 }
 
-async function getProcess() {
+/***********************
+ *                     *
+ *   Event Handlers    *
+ *                     *
+ ***********************/
+
+async function getProcessUpdate() {
   const size = await app.call('size').toPromise()
   const processes = []
   for (let i = 0; i < size; i++) {
