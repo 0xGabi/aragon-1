71ae4e386e69a698feeec38f505e685f3ab3cf6e
Benz <ruthamnong.thotsaphon@gmail.com>
2020-03-20 14:02:24 +0700

    Add eventList file

---
+++ app/src/utils/eventsList.js
@@ -0,0 +1,7 @@
+export default {
+  eventsList: [
+    { name: 'Zapier', instanceHash: '', taskKey: ['execute'] },
+    { name: 'Twitter', instanceHash: '', taskKey: ['tweet'] },
+    { name: 'Webhook', instanceHash: '', taskKey: ['call'] }
+  ]
+}
