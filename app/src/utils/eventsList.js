bd9a2f8f6b2eca045e53930508be3a2cae449eb2
Benz <ruthamnong.thotsaphon@gmail.com>
2020-03-26 18:52:10 +0700

    Update service eventsList

--- app/src/utils/eventsList.js
+++ app/src/utils/eventsList.js
@@ -1,7 +1,7 @@
 export default {
   eventsList: [
     { name: 'Zapier', instanceHash: '', taskKey: 'execute', inputsFields: [] },
-    { name: 'Twitter', instanceHash: '', taskKey: 'tweet', inputsFields: [] },
+    { name: 'Telegram', instanceHash: '', taskKey: 'tweet', inputsFields: [] },
     { name: 'Webhook', instanceHash: process.env.WEBHOOK_HASH, taskKey: 'call', inputsFields: [{ name: 'hookUrl', label: 'Webhook Url', type: 'text' }] }
   ]
 }
