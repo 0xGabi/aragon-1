d47e7a63555c0191de2ce412f5981467d752cc79
Benz <ruthamnong.thotsaphon@gmail.com>
2020-03-27 18:13:44 +0700

    Fix webhook template

--- app/src/utils/ConnectionTemplate.js
+++ app/src/utils/ConnectionTemplate.js
@@ -1,29 +1,22 @@
-import { process } from '@mesg/compiler'
 import { save } from './ipfs-util'
 
 // Template
 import webhook from './template/webhook'
 
-const compileAndSave = async temp => {
-  const compile = await process(Buffer.from(temp))
-  const ipfsHash = await save(JSON.stringify(compile))
-  return ipfsHash
-}
-
 export default async (temp, data) => {
   switch (temp) {
     case 'Webhook':
       try {
-        const temp = await webhook(data)
-        const hash = await compileAndSave(temp)
+        const compiler = await webhook(data)
+        const hash = await save(JSON.stringify(compiler))
         return hash
       } catch (error) {
         throw new Error(error)
       }
     case 'Telegram':
       try {
-        const temp = await webhook(data)
-        const hash = await compileAndSave(temp)
+        const compiler = await webhook(data)
+        const hash = await save(JSON.stringify(compiler))
         return hash
       } catch (error) {
         throw new Error(error)
