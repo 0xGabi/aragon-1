2ba0b802abf29aada1d373acd8d86f36926759d5
Benz <ruthamnong.thotsaphon@gmail.com>
2020-03-30 17:16:55 +0700

    Add zapier temp and updat service list

--- app/src/utils/eventsList.js
+++ app/src/utils/eventsList.js
@@ -1,6 +1,6 @@
 export default {
   eventsList: [
-    { name: 'Zapier', instanceHash: '', taskKey: 'execute', inputsFields: [] },
+    { name: 'Zapier', instanceHash: process.env.ZAPIER_HASH, taskKey: 'execute', inputsFields: [{ name: 'zapName', label: 'Name your zap', type: 'text' }] },
     { name: 'Telegram', instanceHash: process.env.TELEGRAM_HASH, taskKey: 'notify', inputsFields: [{ name: 'sendText', label: 'Message to Notification', type: 'text' }] },
     { name: 'Webhook', instanceHash: process.env.WEBHOOK_HASH, taskKey: 'call', inputsFields: [{ name: 'hookUrl', label: 'Webhook Url', type: 'text' }] }
   ]
---
+++ app/src/utils/template/zapier.js
@@ -0,0 +1,46 @@
+import { process } from '@mesg/compiler'
+
+export default async ({ name, appAddress, eventAbi, mesgAddress, eventSignature, instanceHash }) => {
+  const temp = `
+name: ${name}-${mesgAddress}-${eventAbi.name}
+steps:
+- type: trigger
+  instanceHash: ${process.env.ETHEREUM_HASH}
+  eventKey: log
+- type: filter
+  conditions:
+    address: '${appAddress.toLowerCase()}'
+    eventSignature: '${eventSignature}'
+- type: task
+  instanceHash: ${process.env.ETHEREUM_HASH}
+  taskKey: decodeLog
+  inputs:
+    eventAbi: ${JSON.stringify(eventAbi)}
+    address: {key: address}
+    data: {key: data}
+    topics: {key: topics}
+    logIndex: {key: logIndex}
+    transactionHash: {key: transactionHash}
+    transactionIndex: {key: transactionIndex}
+    blockHash: {key: blockHash}
+    blockNumber: {key: blockNumber}
+- type: task
+  instanceHash: ${instanceHash}
+  taskKey: call
+  inputs:
+    triggerType: aragonTrigger
+    data:
+      decodedData: {key: decodedData}
+      address: {key: address}
+      eventSignature: {key: eventSignature}
+      data: {key: data}
+      topics: {key: topics}
+      logIndex: {key: logIndex}
+      transactionHash: {key: transactionHash}
+      transactionIndex: {key: transactionIndex}
+      blockHash: {key: blockHash}
+      blockNumber: {key: blockNumber}
+  `
+  const compiler = await process(Buffer.from(temp))
+  return compiler
+}
