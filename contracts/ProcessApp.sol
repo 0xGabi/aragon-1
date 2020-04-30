3c79c58f692a2ea7b9d6a81d7ac2c08717373b8b
Benz <ruthamnong.thotsaphon@gmail.com>
2020-04-30 18:37:11 +0700

    update name on ProcessApp.sol

--- contracts/ProcessApp.sol
+++ contracts/ProcessApp.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.24;
 import "@aragon/os/contracts/apps/AragonApp.sol";
 
 contract ProcessApp is AragonApp {
-    event MESGProcessCreate(string ipfsHash, address organization);
-    event MESGProcessDelete(string ipfsHash, address organization);
+    event MESGProcessCreate(string processUrl, address projectName);
+    event MESGProcessDelete(string processUrl, address projectName);
     // Events
     event Created(
         address appAddress,
-        string ipfsHash,
+        string processUrl,
         string eventName,
         string serviceName,
         string data
@@ -18,7 +18,7 @@ contract ProcessApp is AragonApp {
     struct Process {
         uint256 createdAt;
         address appAddress;
-        string ipfsHash;
+        string processUrl;
         string eventName;
         string serviceName;
         string data;
@@ -41,27 +41,27 @@ contract ProcessApp is AragonApp {
      */
     function create(
         address appAddress,
-        string ipfsHash,
+        string processUrl,
         string eventName,
         string serviceName,
         string data,
-        address organization
+        address projectName
     ) external isInitialized auth(PUBLISH_ROLE) {
         process.push(
             Process({
                 createdAt: block.timestamp,
                 appAddress: appAddress,
-                ipfsHash: ipfsHash,
+                processUrl: processUrl,
                 eventName: eventName,
                 serviceName: serviceName,
                 data: data,
                 active: true
             })
         );
-        emit MESGProcessCreate(ipfsHash, organization);
+        emit MESGProcessCreate(processUrl, projectName);
         emit Created(
             appAddress,
-            ipfsHash,
+            processUrl,
             eventName,
             serviceName,
             data
@@ -71,9 +71,9 @@ contract ProcessApp is AragonApp {
     /**
      * @notice Desactivate a process
      */
-    function deacivate(uint256 index, address organization) external auth(DEACTIVATE_ROLE) {
+    function deacivate(uint256 index, address projectName) external auth(DEACTIVATE_ROLE) {
         process[index].active = false;
-        emit MESGProcessDelete(process[index].ipfsHash, organization);
+        emit MESGProcessDelete(process[index].processUrl, projectName);
         emit Deactivated(index);
     }
 
@@ -83,7 +83,7 @@ contract ProcessApp is AragonApp {
         returns (
             uint256 createdAt,
             address appAddress,
-            string ipfsHash,
+            string processUrl,
             string eventName,
             string serviceName,
             string data,
@@ -93,7 +93,7 @@ contract ProcessApp is AragonApp {
         return (
             process[index].createdAt,
             process[index].appAddress,
-            process[index].ipfsHash,
+            process[index].processUrl,
             process[index].eventName,
             process[index].serviceName,
             process[index].data,
