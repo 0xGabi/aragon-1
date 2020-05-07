1b2da5dd96d99e8bcf0392cf7416ebc9b504336a
Benz <ruthamnong.thotsaphon@gmail.com>
2020-05-07 16:03:45 +0700

    Add buidler.config.js and buidler.hook.js

---
+++ buidler.config.js
@@ -0,0 +1,31 @@
+const { usePlugin } = require('@nomiclabs/buidler/config')
+const hooks = require('./scripts/buidler-hooks')
+
+usePlugin('@aragon/buidler-aragon')
+
+module.exports = {
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545'
+    }
+  },
+  solc: {
+    version: '0.4.24',
+    optimizer: {
+      enabled: true,
+      runs: 10000
+    }
+  },
+  etherscan: {
+    apiKey: '' // API Key for smart contract verification. Get yours at https://etherscan.io/apis
+  },
+  aragon: {
+    appServePort: 8001,
+    clientServePort: 9000,
+    appSrcPath: 'app/',
+    appBuildOutputPath: 'dist/',
+    appName: 'mesg',
+    hooks // Path to script hooks
+  }
+}
