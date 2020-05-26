afd922ec64ef9d8669932a6b1fbcd90f34df25a1
Benz <ruthamnong.thotsaphon@gmail.com>
2020-05-26 18:28:34 +0700

    migrate to new version of aragon

--- scripts/buidler-hooks.js
+++ scripts/buidler-hooks.js
@@ -1,7 +1,7 @@
 let token
 let voting
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-
+let accounts
 module.exports = {
   // Called before a dao is deployed.
   preDao: async ({ log }, { web3, artifacts }) => {},
@@ -9,7 +9,7 @@ module.exports = {
   // Called after a dao is deployed.
   postDao: async ({ dao, _experimentalAppInstaller, log }, bre) => {
     const bigExp = (x, y) => bre.web3.utils.toBN(x).mul(bre.web3.utils.toBN(10).pow(bre.web3.utils.toBN(y)))
-    pct16 = x => bigExp(x, 16)
+    const pct16 = x => bigExp(x, 16)
     accounts = await bre.web3.eth.getAccounts()
 
     // Deploy a minime token an generate tokens to root account
@@ -34,11 +34,10 @@ module.exports = {
 
   // Called after the app's proxy is initialized.
   postInit: async ({ proxy, _experimentalAppInstaller, log }, { web3, artifacts }) => {
-    console.log(proxy)
-    await token.createPermission('MINT_ROLE', proxy.address)
-    log(`> MINT_ROLE assigned to ${proxy.address}`)
-    await voting.createPermission('CREATE_VOTES_ROLE', proxy.address)
-    log(`> CREATE_VOTES_ROLE assigned to ${proxy.address}`)
+    await token.createPermission('MINT_ROLE', accounts[0])
+    log(`> MINT_ROLE assigned to ${accounts[0]}`)
+    await voting.createPermission('CREATE_VOTES_ROLE', token.address)
+    log(`> CREATE_VOTES_ROLE assigned to ${token.address}`)
   },
 
   // Called when the start task needs to know the app proxy's init parameters.
